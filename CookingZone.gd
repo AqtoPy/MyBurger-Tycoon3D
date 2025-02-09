7b5549761f5dc3ced3e52786bef0aa0e9dcd6af9
AqtoPy <rekikreki66@gmail.com>
2025-02-09 14:26:30 +0300

    Update CookingZone.gd

--- CookingZone.gd
+++ CookingZone.gd
@@ -1,20 +1,25 @@
 extends Area3D
 
-# Текущее блюдо
 var current_recipe = ""
+var cooking_progress = 0.0
 
-# Функция для начала приготовления
-func start_cooking(recipe_name):
-    if current_recipe == "":
-        current_recipe = recipe_name
-        UI.show_message("Начато приготовление: " + recipe_name)
-        await get_tree().create_timer(5).timeout  # Имитация приготовления
-        UI.show_message("Приготовлено: " + recipe_name)
-        spawn_food(recipe_name)  # Создаём готовое блюдо
-        current_recipe = ""
+func _on_body_entered(body):
+    if body.name == "Player":
+        start_cooking()
 
-# Функция для создания готового блюда
-func spawn_food(recipe_name):
-    var food_scene = load("res://FoodScenes/" + recipe_name + ".tscn")  # Загружаем сцену блюда
-    var food_instance = food_scene.instantiate()
-    add_child(food_instance)
+func start_cooking():
+    var recipe = GameManager.selected_recipe
+    if recipe:
+        current_recipe = recipe
+        $ProgressBar.visible = true
+        $Timer.start(recipe.cook_time)
+
+func _on_timer_timeout():
+    spawn_food(current_recipe)
+    current_recipe = ""
+    $ProgressBar.visible = false
+
+func spawn_food(recipe):
+    var food = preload("res://food.tscn").instantiate()
+    food.init(recipe)
+    add_child(food)
