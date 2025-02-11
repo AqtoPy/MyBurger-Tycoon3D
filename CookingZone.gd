9a5334008b660645b10fccdd28d58d3bf9b27f1f
AqtoPy <rekikreki66@gmail.com>
2025-02-11 22:07:27 +0300

    Update CookingZone.gd

--- CookingZone.gd
+++ CookingZone.gd
@@ -1,41 +1,43 @@
 extends Area3D
 
-var current_recipe = ""
-var cooking_progress = 0.0
+var is_cooking: bool = false
+var current_recipe: String = ""
 
-@onready var equipment = get_parent()  # Ссылка на оборудование
+@onready var progress_bar = $ProgressBar
+@onready var timer = $Timer
 
+# Игрок вошёл в зону
 func _on_body_entered(body):
-    if body.name == "Player":
-        if equipment.can_cook(GameManager.selected_recipe):
-            start_cooking(GameManager.selected_recipe)
+    if body.name == "Player" and !is_cooking:
+        var selected_recipe = GameManager.selected_recipe
+        if get_parent().can_cook(selected_recipe):
+            start_cooking(selected_recipe)
         else:
-            UI.show_message("Это оборудование не подходит для выбранного рецепта!")
+            UI.show_message("Здесь нельзя готовить этот рецепт!")
 
-func start_cooking():
-    var recipe = GameManager.selected_recipe
-    if recipe:
-        current_recipe = recipe
-        $ProgressBar.visible = true
-        $Timer.start(recipe.cook_time)
+# Начать готовку
+func start_cooking(recipe_name: String):
+    is_cooking = true
+    current_recipe = recipe_name
+    progress_bar.visible = true
+    progress_bar.value = 0
+    timer.wait_time = GameManager.recipes[recipe_name]["cook_time"]
+    timer.start()
 
+# Обновление прогресс-бара
+func _process(delta):
+    if is_cooking:
+        progress_bar.value = (1 - timer.time_left / timer.wait_time) * 100
+
+# Готовка завершена
 func _on_timer_timeout():
+    is_cooking = false
+    progress_bar.visible = false
     spawn_food(current_recipe)
-    current_recipe = ""
-    $ProgressBar.visible = false
 
-func spawn_food(recipe_name):
+# Создать блюдо
+func spawn_food(recipe_name: String):
     var food_scene = load("res://FoodScenes/" + recipe_name + ".tscn")
-    var food_instance = food_scene.instantiate()
-    add_child(food_instance)
-    
-    # Передаём еду посетителю
-    var customer = get_nearest_customer()
-    if customer:
-        customer.receive_food(recipe_name)
-
-func get_nearest_customer():
-    var customers = get_tree().get_nodes_in_group("customers")
-    if customers.size() > 0:
-        return customers[0]  # Возвращаем первого посетителя
-    return null
+    var food = food_scene.instantiate()
+    get_parent().add_child(food)
+    food.global_position = $SpawnPoint.global_position  # Точка спавна блюда
