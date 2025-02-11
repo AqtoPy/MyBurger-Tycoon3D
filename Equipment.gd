96cbbed1a52aeed0b076582ea6aa0c796ca04a6e
AqtoPy <rekikreki66@gmail.com>
2025-02-11 22:07:00 +0300

    Update Equipment.gd

--- Equipment.gd
+++ Equipment.gd
@@ -1,14 +1,28 @@
-extends Area3D
+extends Node3D
 
-@export var recipes: Array[String]  # Рецепты, которые можно готовить на этом оборудовании
-@export var equipment_name: String  # Название оборудования
+@export var unlock_price: int = 1000  # Цена разблокировки
+@export var recipes: Array[String] = []  # Рецепты для этого оборудования
+@export var is_locked: bool = true  # Заблокировано ли оборудование
+
+@onready var equipment_mesh = $EquipmentMesh
+@onready var unlock_label = $UnlockPriceLabel
+@onready var cooking_zone = $CookingZone
+@onready var progress_bar = $ProgressBar
 
 func _ready():
-    visible = false  # Оборудование изначально невидимо
+    if is_locked:
+        equipment_mesh.visible = false
+        unlock_label.text = str(unlock_price) + " руб."
+    else:
+        unlock_label.visible = false
 
+# Функция разблокировки
 func unlock():
-    visible = true  # Делаем оборудование видимым
-    $CookingZone.enabled = true  # Активируем зону готовки
+    is_locked = false
+    equipment_mesh.visible = true
+    unlock_label.visible = false
+    cooking_zone.monitoring = true  # Активируем зону готовки
 
-func can_cook(recipe_name):
-    return recipe_name in recipes  # Проверяем, можно ли готовить этот рецепт
+# Проверка, можно ли готовить рецепт
+func can_cook(recipe_name: String) -> bool:
+    return recipe_name in recipes
